2e6cc22e21e34d103111a3fca54c9f15fa240a86
Cédric <60275035+CedricLo@users.noreply.github.com>
2021-10-21 13:04:07 +0200

    init

---
+++ app/src/main/java/com/example/apppotager/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.apppotager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
